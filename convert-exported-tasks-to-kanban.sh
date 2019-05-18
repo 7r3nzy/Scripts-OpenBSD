62970290cfe7b4548ec101b7e03d81e996b272bc
Abdul Rehman <>
2019-05-18 10:24:36 +0500

    Add task warrior exported tasks to kanban cards script

---
+++ convert-exported-tasks-to-kanban.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OIFS="$IFS"
+IFS=$'\n'
+FILENAME=$1
+tasks=(`cut -d':' $FILENAME -f 3 | cut -d',' -f 1`)
+tlength=${#tasks[@]}
+
+echo '"cards": ['
+for i in "${tasks[@]:1:tlength-2}"; do
+  echo '  {'
+  printf '\n  "name": %s\n' $i
+  echo '  "details": "", '
+  echo '  "color": "FFFFFF"'
+  echo '  },'
+done
+echo ']'
+
+IFS="$OIFS"
+
